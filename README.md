947b17d50a1065d1c77cdbd763e49fdd8c8f4975
liao <liao961120@gmail.com>
2020-01-13 21:15:25 +0800

    update

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ python3 build.py
 
 This builds the website into root dir.
 
+
 ## Edit Website
 
-All editing need to be done in the html template files in `templates/`.
+All editing are done in html template files in `templates/`
--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,9 @@
 window.onload = function() {
     var selector = `nav#nav > ul > li > a[href="${document.URL.split('/').slice(-1)[0]}"]`
     var active = document.querySelector(selector);
-    active.parentNode.className = 'active';
+
+    if (active == null)
+        var active = document.querySelector('nav#nav > ul > li > a');
+
+    active.parentNode.className = "active";
 }
